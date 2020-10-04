e0b43eeb4884785e29cc8fcc97bf21ba267aac37
Priya Rathinavelu <plr11@duke.edu>
2020-10-04 15:06:36 -0400

    created initial simulation and controller main classes

---
+++ src/controller/ControllerMain.java
@@ -0,0 +1,84 @@
+package controller;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.util.Duration;
+
+public class ControllerMain extends Application {
+
+  public static final int FRAMES_PER_SECOND = 60;
+  public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+  public static final int FRAME_SIZE = 400;
+  public static final Paint BACKGROUND = Color.AZURE;
+  private Scene myScene;
+  private Group root;
+
+  @Override
+  public void start(Stage stage){
+    // attach scene to the stage and display it
+    setUpStage(stage);
+    KeyFrame frame = new KeyFrame(Duration.seconds(SECOND_DELAY), e -> step(SECOND_DELAY));
+    Timeline animation = new Timeline();
+    animation.setCycleCount(Timeline.INDEFINITE);
+    animation.getKeyFrames().add(frame);
+    animation.play();
+  }
+
+  /*
+   * Sets up the stage size and title
+   */
+  protected void setUpStage(Stage stage) {
+    setupScene(FRAME_SIZE, FRAME_SIZE, BACKGROUND);
+    stage.setScene(myScene);
+    stage.setTitle("Testing");
+    stage.show();
+  }
+
+  /*
+   * Create the game's "scene": what shapes will be in the game and their starting properties
+   */
+  Scene setupScene(int width, int height, Paint background) {
+    root = new Group();
+    myScene = new Scene(root, width, height, background);
+    clickButton();
+    //myScene.setOnKeyPressed(e -> handleKeyInput(e.getCode()));
+    return myScene;
+  }
+
+
+  void step (double elapsedTime) {
+    updateShapes(elapsedTime);
+  }
+
+  private void updateShapes(double elapsedTime) {
+
+  }
+
+  private void clickButton() {
+  Button playAgainButton = new Button(" Click to play again");
+    playAgainButton.setLayoutX(FRAME_SIZE / 2.0); // NOTE position button
+    playAgainButton.setLayoutY(FRAME_SIZE / 2.0);
+    HBox hbox = new HBox(playAgainButton);
+    root.getChildren().add(hbox);
+    playAgainButton.setOnAction(actionEvent -> test());
+}
+
+void test() {
+  System.out.println("working");
+}
+
+
+
+  public static void main (String[] args) {
+    launch(args);
+  }
+}
---
+++ src/controller/Simulation.java
@@ -0,0 +1,45 @@
+package controller;
+
+import javafx.scene.Group;
+
+public abstract class Simulation {
+  private Grid currentGrid;
+  private Grid nextGrid;
+  private final String simulationName;
+  private final List<State> possibleStates;
+  private Group root;
+
+
+
+  public Simulation(String SimulationNameType, List<State> simulationStates) {
+    simulationName = SimulationNameType;
+    currentGrid = new Grid();
+    nextGrid = new Grid();
+    possibleStates = simulationStates;
+  }
+
+//CHECK can remove this method if initializing in the constructor itself
+  void initializeSimulation(Group root) { //CHECK might not need to pass root in
+    currentGrid.initalizeGrid(this.simulationName); //CHECK use method in Grid
+    nextGrid = currentGrid.getUpdateGrid(this.simulationName); //CHECK method names?
+    displayGridScene(currentGrid);
+  }
+
+  void displayGridScene(Grid gridToBeDisplayed) {
+    //CHECK depending on how Grid is storing cells
+    //CHECK need to create instance of cellDisplay ?
+    for (int i = 0; i < gridToBeDisplayed.getNumberOfCells(); i++) {
+      cellDisplay.displayCellMethod(gridToBeDisplayed.getCell(i));
+    }
+  }
+
+  void updateSimulationGrid() {
+    this.currentGrid = nextGrid;
+    this.nextGrid = currentGrid.getUpdateGrid(this.simulationName);
+    //Need to update display
+    for (int i = 0; i < currentGrid.getNumberOfCells(); i++) {
+      root.getChildren().remove(currentGrid.getCell(i));
+    }
+    displayGridScene(currentGrid);
+  };
+}
