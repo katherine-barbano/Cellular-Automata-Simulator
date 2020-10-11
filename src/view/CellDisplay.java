a47d65719d5a61d6e5e27958fab36dcc43170748
Heather Grune <heather.grune@duke.edu>
2020-10-11 19:55:07 -0400

    Use state enum in CellDisplay

--- src/view/CellDisplay.java
+++ src/view/CellDisplay.java
@@ -4,25 +4,22 @@ import static controller.GameOfLifeState.ALIVE;
 import static controller.GameOfLifeState.DEAD;
 
 import controller.State;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.shape.Rectangle;
 
 public class CellDisplay extends Rectangle {
 
   public CellDisplay(State state,double cellSize){
     super(cellSize, cellSize);
-    this.setId();
+    this.setId(getIdFromState(state));
     getStyleClass().add("cell-display");
   }
 
   private String getIdFromState(State input){
-    switch(input){
-      case DEAD: return "dead";
-      case ALIVE: return "alive";
-      default: return "none";
+    if (DEAD.equals(input)) {
+      return "dead";
+    } else if (ALIVE.equals(input)) {
+      return "alive";
     }
+    return "none";
   }
-
 }
