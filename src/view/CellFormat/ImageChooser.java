26d5f276e1096db5a46abbe797a5581ed0b18d0b
Heather Grune <heather.grune@duke.edu>
2020-10-11 23:33:40 -0400

    Adding pickImageFile method for ImageChooserButton

--- src/view/CellFormat/ImageChooser.java
+++ src/view/CellFormat/ImageChooser.java
@@ -1,14 +1,20 @@
 package view.CellFormat;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ResourceBundle;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 
 public class ImageChooser extends Button {
 
   public static final String IMAGE_CHOOSER_PROPERTIES = "ImageChooserButton";
+  public static final String IMAGE_PATH_PLACEHOLDER = "data/images/Starsinthesky.jpg";
   private FileChooser myFileChooser;
+  private FileInputStream mySelectedFile;
+  private Image myImage;
 
   public ImageChooser(ResourceBundle resources){
     super();
@@ -21,7 +27,17 @@ public class ImageChooser extends Button {
 
   private void pickImageFile(){
     //File selectedFile = myFileChooser.showOpenDialog(stage);
+    try {
+      mySelectedFile = new FileInputStream(IMAGE_PATH_PLACEHOLDER);
+      myImage = new Image(mySelectedFile);
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException();
+    }
 
+  }
+
+  public Image getChosenImage(){
+    return myImage;
   }
 
 }
