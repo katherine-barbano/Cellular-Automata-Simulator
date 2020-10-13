80d56c14d6b6dc6b5e089d88835c37c04ce05a64
Heather Grune <heather.grune@duke.edu>
2020-10-12 20:19:40 -0400

    Getting images from Properties file

--- src/view/CellFormat/ImageChooser.java
+++ src/view/CellFormat/ImageChooser.java
@@ -1,7 +1,5 @@
 package view.CellFormat;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 import javafx.beans.value.ChangeListener;
@@ -10,19 +8,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Tooltip;
-import javafx.scene.image.Image;
+import javafx.scene.paint.Paint;
 
 public class ImageChooser extends ChoiceBox {
-
-  public static final String IMAGE_CHOOSER_PROPERTIES = "ImageChooserButton";
+  public static final String[] IMAGE_PROPERTIES = {"StarryNightImage","PolkaDotImage"};
   public static final CellColors[] CELL_PHOTOS = {CellColors.STARRY_NIGHT,CellColors.POLKA_DOTS};
   public static final String[] CELL_PHOTO_NAMES = {"Starry Night", "Polka Dots"};
-  private FileInputStream mySelectedFile;
+  private Paint[] myImagePatterns = new Paint[IMAGE_PROPERTIES.length];
   private ObservableList myPhotos;
   private CellColors myChosenImage;
 
   public ImageChooser(ResourceBundle resources){
     super();
+
+    getImagesFromResources(resources);
 
     this.setTooltip(new Tooltip("Select a Photo: "));
 
@@ -41,5 +40,13 @@ public class ImageChooser extends ChoiceBox {
   public CellColors getChosenImage(){
     return myChosenImage;
   }
+
+  private void getImagesFromResources(ResourceBundle resources){
+    for(int fileNum=0;fileNum<myImagePatterns.length;fileNum++){
+      String filePath = resources.getString(IMAGE_PROPERTIES[fileNum]);
+      CELL_PHOTOS[fileNum].setCellImage(filePath);
+    }
+  }
+
 
 }
