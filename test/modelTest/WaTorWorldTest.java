340ebe8a944574666711a2220ee5135e0a43558f
Katherine Barbano <katherine.barbano@duke.edu>
2020-10-14 00:19:35 -0400

    wator world movement working with tests

--- test/modelTest/WaTorWorldTest.java
+++ test/modelTest/WaTorWorldTest.java
@@ -17,18 +17,97 @@ class WaTorWorldTest {
   }
 
   @Test
-  void getNextGridWaTorWorldAllFish() {
+  void getNextGridWaTorWorldFishMovement() {
     State[][] grid = new State[][] {
         {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
         {WaTorWorldState.EMPTY, WaTorWorldState.FISH, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
         {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
     };
 
+    State[][] possibleOutcome1 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.FISH, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome2 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.FISH, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome3 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.FISH, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome4 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.FISH, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
     Grid currentGrid = new Grid(SimulationType.WATOR_WORLD, grid);
     Grid actualNextGrid = currentGrid.getNextGrid();
+
+    Grid expected1 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome1);
+    Grid expected2 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome2);
+    Grid expected3 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome3);
+    Grid expected4 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome4);
+
+    assertTrue(actualNextGrid.equals(expected1) || actualNextGrid.equals(expected2) || actualNextGrid.equals(expected3) || actualNextGrid.equals(expected4));
+  }
+
+  @Test
+  void getNextGridWaTorWorldSharkMovement() {
+    State[][] grid = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.SHARK, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome1 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.SHARK, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome2 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.SHARK, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome3 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.SHARK, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    State[][] possibleOutcome4 = new State[][] {
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.SHARK, WaTorWorldState.EMPTY},
+        {WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY, WaTorWorldState.EMPTY}
+    };
+
+    Grid currentGrid = new Grid(SimulationType.WATOR_WORLD, grid);
+    Grid actualNextGrid = currentGrid.getNextGrid();
+
+    Grid expected1 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome1);
+    Grid expected2 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome2);
+    Grid expected3 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome3);
+    Grid expected4 = new Grid(SimulationType.WATOR_WORLD, possibleOutcome4);
+
+    assertTrue(actualNextGrid.equals(expected1) || actualNextGrid.equals(expected2) || actualNextGrid.equals(expected3) || actualNextGrid.equals(expected4));
+  }
+
+
+  //for help debugging
+  private void printGrid(Grid grid) {
     for(int r = 0; r<3; r++) {
       for(int c = 0; c<4; c++) {
-        System.out.print(actualNextGrid.getCell(r,c).getCurrentState());
+        System.out.print(grid.getCell(r,c).getCurrentState());
       }
       System.out.println();
     }
