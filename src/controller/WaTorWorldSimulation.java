df2ff7df3a9cbfdbbe6a46bfb4a616de89c82188
Heather Grune <heather.grune@duke.edu>
2020-10-19 14:33:52 -0400

    Changing WaTorWorldSimulation name to use uppercase T

--- src/controller/WaTorWorldSimulation.java
+++ src/controller/WaTorWorldSimulation.java
@@ -9,7 +9,7 @@ import java.util.Properties;
 import java.util.ResourceBundle;
 import model.Grid;
 
-public class WatorWorldSimulation extends Simulation{
+public class WaTorWorldSimulation extends Simulation{
 
   private static final String STORING_FILE_NAME = "data/outputGrids/watorWorld";
   private int numberRows;
@@ -18,7 +18,7 @@ public class WatorWorldSimulation extends Simulation{
   private HashMap<StateType, Integer> integerForStates;
   ;
 
-  public WatorWorldSimulation() {
+  public WaTorWorldSimulation() {
     super("WatorWorld");
   }
 
