048ee9bf6107e200c822d88060eefa9150220d88
Heather Grune <heather.grune@duke.edu>
2020-10-19 01:18:54 -0400

    First Try at fixing bug speed up/slow down bug that was restarting graph from 0

--- src/controller/Simulation.java
+++ src/controller/Simulation.java
@@ -47,14 +47,15 @@ public abstract class Simulation {
   public Simulation(String newSimulationName){
       this.simulationName = newSimulationName;
       this.propertiesInformation = new HashMap<String, String>();
-      readPropertiesFile(newSimulationName);
-      simulationFileLocation =
-          "data/initialConfigurations/" + propertiesInformation.get("fileName");
-      //simulationFileLocation = "data/initialConfigurations/testingGOL.csv";
+      //readPropertiesFile(newSimulationName);
+      //simulationFileLocation =
+          //"data/initialConfigurations/" + propertiesInformation.get("fileName");
+      simulationFileLocation = "data/initialConfigurations/testingGOL.csv";
       this.possibleStateTypes = getStateTypesForSimulation();
-      currentGrid = new Grid(simulationName, propertiesInformation.get("edgePolicy"),
-          propertiesInformation.get("neighborPolicy"),
-          createStates(readCellStatesFile(), possibleStateTypes));
+      //currentGrid = new Grid(simulationName, propertiesInformation.get("edgePolicy"),
+          //propertiesInformation.get("neighborPolicy"),
+          //createStates(readCellStatesFile(), possibleStateTypes));
+    currentGrid = new Grid(simulationName, "Finite","Complete",createStates(readCellStatesFile(), possibleStateTypes));
       nextGrid = currentGrid.getNextGrid();
    // simulationView = new SimulationView(currentGrid);
   }
