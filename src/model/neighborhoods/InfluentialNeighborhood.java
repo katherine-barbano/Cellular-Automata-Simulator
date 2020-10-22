ea867f7669353e0d9e2d10e7b35222fdc8092e83
Katherine Barbano <katherine.barbano@duke.edu>
2020-10-22 18:03:32 -0400

    javadoc influential noninfluential neighborhoods

--- src/model/neighborhoods/InfluentialNeighborhood.java
+++ src/model/neighborhoods/InfluentialNeighborhood.java
@@ -5,8 +5,23 @@ import java.util.List;
 import model.NeighborPolicy;
 import model.Neighborhood;
 
+/***
+ * Represents a Neighborhood that must influence and be influenced by other Neighborhoods
+ * in the Grid. This provides functionality for movement and maintaining States/age
+ * between Grids in WaTorWorld and Segregation.
+ *
+ * Provides helpers to allow conflicts to be resolved between neighborhoods of neighbors
+ * in the second step of the nextGrid retrieval while still giving the needed flexibility by
+ * maintaining abstractions for getNextState and getStateOfOverlappingNeighbors.
+ *
+ * @author Katherine Barbano
+ */
 public abstract class InfluentialNeighborhood extends Neighborhood {
 
+  /***
+   * Overrides constructor for Neighborhood
+   * @param neighborPolicy NeighborPolicy object
+   */
   public InfluentialNeighborhood(NeighborPolicy neighborPolicy) {
     super(neighborPolicy);
   }
@@ -20,6 +35,11 @@ public abstract class InfluentialNeighborhood extends Neighborhood {
     return new State(neighborState.getStateType());
   }
 
+  /***
+   * Helper to delete a state from the neighborhoods of neighbors and replace it with
+   * another State object (usually EMPTY).
+   * @param newState new State to replace the old state
+   */
   public void deleteMovedStateFromNeighborhoodsOfNeighbors(State newState) {
     for(int[] neighborPosition : getNeighborhoodsOfNeighbors().keySet()) {
       int[] positionOfCenterCellInNeighbor = negateArray(neighborPosition);
--- src/model/neighborhoods/NonInfluentialNeighborhood.java
+++ src/model/neighborhoods/NonInfluentialNeighborhood.java
@@ -5,12 +5,34 @@ import java.util.Map;
 import model.NeighborPolicy;
 import model.Neighborhood;
 
+/***
+ * Represents a Neighborhood that does not need to influence and be influenced by other Neighborhoods
+ * in the Grid. These Neighborhoods, such as GameOfLifeNeighborhood, PercolationNeighborhood,
+ * RockPaperScissorsNeighborhood, and SpreadingOfFireNeighborhood implement getNextState using information about
+ * the States of only ITS OWN neighbors, not from other neighborhoods.
+ *
+ * This class defines getStateOfOverlappingNeighbors to reduce code duplication.
+ *
+ * @author Katherine Barbano
+ */
 public abstract class NonInfluentialNeighborhood extends Neighborhood {
 
+  /***
+   * Constructor overrides neighborhood constructor
+   * @param neighborPolicy Neighbor policy object
+   */
   public NonInfluentialNeighborhood(NeighborPolicy neighborPolicy) {
     super(neighborPolicy);
   }
 
+  /***
+   * Because in these neighborhoods, the next state is determined only using information about
+   * the States of only ITS OWN neighbors, not from other neighborhoods, there will be no conflicts
+   * that arise. Thus, simply returning the nextState as determined by its own neighborhood is sufficient.
+   * @param nextState The proposed nextState returned by getNextState, only from the info in Cell's own neighborhood
+   * @param statesOfOverlappingNeighborsOnCell A Map of keys with int[] relative positions, and values of States in those positions in the Grid
+   * @return
+   */
   @Override
   public State getStateOfOverlappingNeighbors(State nextState, Map<int[], State> statesOfOverlappingNeighborsOnCell) {
     return nextState;
