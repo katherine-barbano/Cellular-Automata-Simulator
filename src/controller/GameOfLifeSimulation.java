20433ec2f42cf459b9168b07861e08572d8c8f88
Priya Rathinavelu <plr11@duke.edu>
2020-10-22 02:11:37 -0400

    added other javadoc comments to the rest of controller classes

--- src/controller/GameOfLifeSimulation.java
+++ src/controller/GameOfLifeSimulation.java
@@ -12,6 +12,20 @@ import java.util.ResourceBundle;
 import javax.swing.JOptionPane;
 import model.Grid;
 
+/*
+ * This class is the specific class extending the Simulation class that represents the Game of Life
+ * simulation. This class alone assumes that the properties file associated with this
+ * simulation has the name "GameOfLife". If the properties file name is not this, then the
+ * initial set up of the simulation would not be possible because there is no default set up. In
+ * addition, this class assumes that the states available for this simulation are defined within the
+ * GameOfLifeState enum so that the correct formation of states based on the configuration can
+ * be created. An example of how to use this class is to instantiate it when you want to create a
+ * new game of life simulation. In order to actually display it, it will need to be connected
+ * with a simulation view. This class is an extension of the abstract simulation class so it depends
+ * on what that class depends on. In addition, GameOfLifeSimulation needs to work with the
+ * gameOfLifeState to get the states found within that enum.
+ */
+
 public class GameOfLifeSimulation extends Simulation {
 
   private static final String STORING_FILE_NAME = "data/outputGrids/";
@@ -25,95 +39,13 @@ public class GameOfLifeSimulation extends Simulation {
     super("GameOfLife");
   }
 
-/*  @Override
-  public StateType[][] createStatesFromInteger(int[][] integerCellStates) {
-*//*    statesForInteger = new HashMap<>();
-    integerForStates = new HashMap<>();
-    StateType possibleStatesInGameOfLife[] = GameOfLifeState.values();
-    int stateNumber = 0;
-    for(StateType state : possibleStatesInGameOfLife) {
-      integerForStates.put(state, stateNumber);
-      statesForInteger.put(stateNumber,state);
-      stateNumber++;
-    }
-    StateType[][] cellStates = new StateType[integerCellStates.length][integerCellStates[0].length];
-    numberRows = integerCellStates.length;
-    numberCols = integerCellStates[0].length;
-    for (int row = 0; row < integerCellStates.length; row++) {
-      for (int col = 0; col < integerCellStates[0].length; col++) {
-        cellStates[row][col] = statesForInteger.get(integerCellStates[row][col]);
-      }
-    }
-    return cellStates;*//*
-    return null;
-  }*/
-
-/*  @Override
-  public String readInPropertiesFile() {
-    try {
-      Properties myProperties = new Properties();
-      InputStream is = GameOfLifeSimulation.class.getClass().getResourceAsStream("ControllerError.properties");
-      myProperties.load(is);
-      System.out.println("loaded now");
-      return "";
-    }
-    catch (Exception e) {
-      System.out.println("exception");
-    }
-    return null;
-  }*/
-
+  /*
+  This method returns the state types specific to the game of life simulation by getting the game of
+  life state's values from that enum
+   */
   @Override
   public StateType[] getStateTypesForSimulation() {
     return GameOfLifeState.values();
   }
-/*
-  @Override
-  public void storeNewCellConfig(Grid gridToStore) {*/
-/*      try {
-        String input = JOptionPane.showInputDialog("Enter new File name (with csv)");
-        File file = new File(input);
-        FileWriter csvWriter = new FileWriter(STORING_FILE_NAME+ file.getName());
-        //FileWriter csvWriter = new FileWriter(file.getName());
-        csvWriter.append(Integer.toString(numberRows));
-        csvWriter.append(",");
-        csvWriter.append(Integer.toString(numberCols));
-        csvWriter.append(",");
-        csvWriter.append("\n");
-
-        for(int row=0; row<gridToStore.getGridNumberOfRows(); row++){
-          for(int col=0; col<gridToStore.getGridNumberOfColumns();col++) {
-            State x = gridToStore.getCell(row,col).getCurrentState();
-            Integer t = integerForStates.get(gridToStore.getCell(row,col).getCurrentState());
-            String s = t.toString();
-            csvWriter.append(integerForStates.get(gridToStore.getCell(row,col).getCurrentState()).toString());
-            //csvWriter.append(gridToStore.getCell(row,col).getCurrentState().toString());
-            csvWriter.append(",");
-          }
-          csvWriter.append("\n");
-        }
-        csvWriter.flush();
-        csvWriter.close();
-        String resourceName = "simulationProperties/GameOfLife.properties"; // could also be a constant
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        Properties props = new Properties();
-        try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
-          props.load(resourceStream);
-        }
-        Object s = "fileName";
-        props.replace(s, props.get(s), input);
-        FileOutputStream out = new FileOutputStream(
-            String.valueOf(loader.getResourceAsStream(resourceName)));
-        props.setProperty("fileName", file.getName());
-        props.store(out, null);
-        out.close();
-        System.out.println("done");
-
-      } catch (IOException e) {
-        //System.out.println("not working");
-        String invalidFileExceptionMessage = ResourceBundle.getBundle("resources/ControllerErrors").
-            getString("InvalidFile");
-        throw new ControllerException(invalidFileExceptionMessage);
-      }*/
 
 }
--- src/controller/RockPaperScissorsSimulation.java
+++ src/controller/RockPaperScissorsSimulation.java
@@ -9,6 +9,20 @@ import java.util.Properties;
 import java.util.ResourceBundle;
 import model.Grid;
 
+/*
+ * This class is the specific class extending the Simulation class that represents the Rock Paper
+ * Scissors simulation. This class alone assumes that the properties file associated with this
+ * simulation has the name "RockPaperScissors". If the properties file name is not this, then the
+ * initial set up of the simulation would not be possible because there is no default set up. In
+ * addition, this class assumes that the states available for this simulation are defined within the
+ * RockPaperScissorsState enum so that the correct formation of states based on the configuration can
+ * be created. An example of how to use this class is to instantiate it when you want to create a
+ * new rock paper scissors simulation. In order to actually display it, it will need to be connected
+ * with a simulation view. This class is an extension of the abstract simulation class so it depends
+ * on what that class depends on. In addition, RockPaperScissorsSimulation needs to work with the
+ * rockPaperScissorsState to get the states found within that enum.
+ */
+
 public class RockPaperScissorsSimulation extends Simulation{
   private static final String STORING_FILE_NAME = "data/outputGrids/rockPaperScissors";
   private int numberRows;
@@ -21,76 +35,15 @@ public class RockPaperScissorsSimulation extends Simulation{
     super("RockPaperScissors");
   }
 
-/*  @Override
-  public StateType[][] createStatesFromInteger(int[][] integerCellStates) {
-*//*    statesForInteger = new HashMap<>();
-    integerForStates = new HashMap<>();
-    StateType possibleStatesInGameOfLife[] = RockPaperScissorsState.values();
-    int stateNumber = 0;
-    for(StateType state : possibleStatesInGameOfLife) {
-      integerForStates.put(state, stateNumber);
-      statesForInteger.put(stateNumber,state);
-      stateNumber++;
-    }
-    StateType[][] cellStates = new StateType[integerCellStates.length][integerCellStates[0].length];
-    numberRows = integerCellStates.length;
-    numberCols = integerCellStates[0].length;
-    for (int row = 0; row < integerCellStates.length; row++) {
-      for (int col = 0; col < integerCellStates[0].length; col++) {
-        cellStates[row][col] = statesForInteger.get(integerCellStates[row][col]);
-      }
-    }
-    return cellStates;*//*
-    return null;
-  }*/
 
-/*  @Override
-  public String readInPropertiesFile() {
-    try {
-      Properties myProperties = new Properties();
-      InputStream is = GameOfLifeSimulation.class.getClass().getResourceAsStream("ControllerError.properties");
-      myProperties.load(is);
-      System.out.println("loaded now");
-      return "";
-    }
-    catch (Exception e) {
-      System.out.println("exception");
-    }
-    return null;
-  }*/
-
+  /*
+  This method returns the state types specific to the rock paper scissors simulation by getting the
+   rock paper scissors state's values from that enum
+ */
   @Override
   public StateType[] getStateTypesForSimulation() {
     return RockPaperScissorsState.values();
   }
 
-/*  @Override
-  public void storeNewCellConfig(Grid gridToStore) {
-    try {
-      FileWriter csvWriter = new FileWriter(STORING_FILE_NAME+"New.csv");
-      csvWriter.append(Integer.toString(numberRows));
-      csvWriter.append(",");
-      csvWriter.append(Integer.toString(numberCols));
-      csvWriter.append(",");
-      csvWriter.append("\n");
-
-      for(int row=0; row<gridToStore.getGridNumberOfRows(); row++){
-        for(int col=0; col<gridToStore.getGridNumberOfColumns();col++) {
-          csvWriter.append(integerForStates.get(gridToStore.getCell(row,col).getCurrentState()).toString());
-          //csvWriter.append(gridToStore.getCell(row,col).getCurrentState().toString());
-          csvWriter.append(",");
-        }
-        csvWriter.append("\n");
-      }
-      csvWriter.flush();
-      csvWriter.close();
-      System.out.println("saved");
-    } catch (IOException e) {
-      //System.out.println("not working");
-      String invalidFileExceptionMessage = ResourceBundle.getBundle("resources/ControllerErrors").
-          getString("InvalidFile");
-      throw new ControllerException(invalidFileExceptionMessage);
-    }
-  }*/
 
 }
